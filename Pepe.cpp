6335633d54d632304ce8112451d3f87279a2a368
Maikk922 <mdelgadomu@unal.edu.co>
2026-05-08 12:21:55 -0500

    Agregue lectura de archivo

--- Pepe.cpp
+++ Pepe.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
-int main(){
-	
+
+int main() {
+    ifstream archivo("input/archivoentrada.txt.txt");
+
+    string linea;
+
+    while(getline(archivo, linea)){
+        cout << linea << endl;
+    }
+
+    archivo.close();
+
+    return 0;
 }
