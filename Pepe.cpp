c8b355067074f29f5d3aea3de2d86b2c2fb6d158
unknown <you@example.com>
2026-05-10 20:53:23 -0500

    añadidas lineas para escribir en archivo

--- Pepe.cpp
+++ Pepe.cpp
@@ -1,21 +1,20 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+
 using namespace std;
+int main () {
+    ifstream archivoEntrada("input/archivoEntrada.txt");
+    ofstream archivoSalida("output/archivoSalida.txt");
 
-int main() {
-    ifstream archivo("input/archivoentrada.txt");
+    if (archivoEntrada.is_open() && archivoSalida.is_open()) {
+    cout<<"Archivo abierto"<<endl;
+    string linea;
 
+    while (getline(archivoEntrada, linea)) {
+    archivoSalida << linea;
+    }
 
-if (archivoentrada.is_open()){
-	cout << "Archivo Abierto" << endl;
-	
-    string linea;	
-    while(getline(archivo, linea)){
-        cout << linea << endl;
-    }}
-
-    archivoentrada.close();
-
-    return 0;
+    cout<<"Archivo copiado correctamente"<<endl;
+    }
 }
